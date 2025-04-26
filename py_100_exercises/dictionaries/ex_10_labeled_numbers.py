5d397eec1bb8d30ad97cd57679c2153ea9c07e3a
Justin <justinr7337@gmail.com>
2025-04-26 14:18:32 -0500

    Added rest of PY100 exercises (end of PY100).

---
+++ py_100_exercises/dictionaries/ex_10_labeled_numbers.py
@@ -0,0 +1,11 @@
+numbers ={
+    'high':100,
+    'medium':50,
+    'low':25,
+    }
+
+# for key in numbers:
+#     print(f'A {key} number is {numbers[key]}.')
+
+for key, value in numbers.items():
+    print(f'A {key} number is {value}.')
